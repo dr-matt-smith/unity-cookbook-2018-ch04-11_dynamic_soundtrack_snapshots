c16e2d2c0225cf663d4268a6f22d5c0f6e76dbd9
dr-matt-smith <dr_matt_smith@me.com>
2017-12-04 16:38:45 +0000

    files added to project

---
+++ Assets/DynamicSoundtrack/_Scripts/SnapshotTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+/* ----------------------------------------
+ * class to demonstrate how to transition between 
+ * Audio mixer Snapshots, making a dynamic soundtrack possible
+ */ 
+public class SnapshotTrigger : MonoBehaviour
+{
+	// reference to SnapShot to be transitioned into
+	public AudioMixerSnapshot snapshot;
+
+    // Duration (in seconds) for the crossfade between snapshots
+	public float crossfade;
+
+	/* ----------------------------------------
+	 * When trigger enter event, begin the crossfade
+	 */
+	private void OnTriggerEnter(Collider other)
+	{
+		// Make a transition to our 'snapshot' in 'crossfade' seconds
+		snapshot.TransitionTo (crossfade);
+	}
+}
